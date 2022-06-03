a1790bf6af5e025ccf6391efef8797d2bcfcd06c
DayLabs18 <daylabspark@gmail.com>
2022-06-03 11:46:40 +0900

    초기 커밋.

---
+++ BoardProject/Controllers/BoardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoardProject.Controllers
+{
+    public class BoardController : Controller
+    {
+        // GET: Board
+        public ActionResult BoardView()
+        {
+            return View();
+        }
+    }
+}
